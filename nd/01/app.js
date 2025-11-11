6403352704e29a65117f165b4c3b14f75a252baf
Rol-Ka <rolandaskakta1@gmail.com>
2025-11-11 15:50:43 +0200

    javascript

--- nd/01/app.js
+++ nd/01/app.js
@@ -17,7 +17,56 @@ h3.innerText = '3';
 const h4 = document.querySelector('h4');
 h4.innerText = '4';
 
-const div = document.querySelector('div');
+const divai = document.querySelectorAll('div');
+
+let div1Color = 'blue';
+
+const kvadratas = divai[0];
+kvadratas.style.width = '150px';
+kvadratas.style.height = '150px';
+kvadratas.style.backgroundColor = div1Color;
+kvadratas.style.display = 'inline-block';
+kvadratas.style.margin = '20px';
+
+
+
+let div2Color = 'red';
+const apskritimas = divai[1];
+
+apskritimas.style.width = '150px';
+apskritimas.style.height = '150px';
+apskritimas.style.backgroundColor = div2Color;
+apskritimas.style.borderRadius = '50%';
+apskritimas.style.display = 'inline-block';
+apskritimas.style.margin = '20px';
+
+
+// const divai = document.querySelectorAll('div');
+
+// let div1Color = 'blue';
+
+// const kvadratas = divai[0];
+// divai[0].style.width = '150px';
+// divai[0].style.height = '150px';
+// divai[0].style.backgroundColor = div1Color;
+// divai[0].style.display = 'inline-block';
+// divai[0].style.margin = '20px';
+
+
+
+// let div2Color = 'red';
+// const apskritimas = divai[1];
+
+// divai[1].style.width = '150px';
+// divai[1].style.height = '150px';
+// divai[1].style.backgroundColor = div2Color;
+// divai[1].style.borderRadius = '50%';
+// divai[1].style.display = 'inline-block';
+// divai[1].style.margin = '20px';
+
+
+
+
 
 
 
