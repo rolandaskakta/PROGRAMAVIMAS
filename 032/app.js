2d63253a23c12757b6be66e36896e4edc566fb96
Rol-Ka <rolandaskakta1@gmail.com>
2025-12-16 13:37:48 +0200

    pamoka

--- 032/app.js
+++ 032/app.js
@@ -1,8 +1,14 @@
-import { v4 as manoUuid } from 'uuid'; // paima ne default metoda v4 ir pavidinu ji manoUUID
-
+import { v4 as manoUuid } from 'uuid'; // paima ne default metoda v4 ir pavadinimu ji manoUUID
+import axios from 'axios';
 // console.log('Labas ka tu?', 'Labas as gerai', 5*5);
 
 
 const naujasId = manoUuid();
 console.log('Sugeneruotas UUID:', naujasId);
  
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+.then(res => console.log(res.data))
