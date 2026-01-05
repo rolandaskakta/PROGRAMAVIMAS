3cac827db0058f61f211128e05656bf14c4c9b11
Rol-Ka <rolandaskakta1@gmail.com>
2026-01-05 23:21:52 +0200

    js

--- 040/index.js
+++ 040/index.js
@@ -8,10 +8,51 @@ const port = 80;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const top = fs.readFileSync('./html/top.html', 'utf8');
-    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
-    const home = fs.readFileSync('./html/home.html', 'utf8');
-    res.send(top + home + bottom);
+
+  //skaito tris atskirus failus, toks skaitymas galimas tik back-ende
+  const top = fs.readFileSync('./html/top.html', 'utf8');
+  const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+  const home = fs.readFileSync('./html/home.html', 'utf8');
+
+  const pageTitle = 'Home Page';
+  const topWithTitle = top.replace('{{title}}', pageTitle);
+
+  res.send(topWithTitle + home + bottom); // jau vienas HTML failas
+});
+
+app.get('/services', (req, res) => {
+
+  //skaito tris atskirus failus, toks skaitymas galimas tik back-ende
+  const top = fs.readFileSync('./html/top.html', 'utf8');
+  const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+  const services = fs.readFileSync('./html/services.html', 'utf8');
+
+  const pageTitle = 'Services';
+  const topWithTitle = top.replace('{{title}}', pageTitle);
+  res.send(topWithTitle + services + bottom); // vienas html failass
+});
+
+app.get('/about', (req, res) => {
+
+  //skaito tris atskirus failus, toks skaitymas galimas tik back-ende
+  const top = fs.readFileSync('./html/top.html', 'utf8');
+  const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+  const about = fs.readFileSync('./html/about.html', 'utf8');
+const pageTitle = 'About us';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+  res.send(topWithTitle + about + bottom); // vienas html failass
+});
+
+app.get('/contact', (req, res) => {
+
+  //skaito tris atskirus failus, toks skaitymas galimas tik back-ende
+  const top = fs.readFileSync('./html/top.html', 'utf8');
+  const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+  const contact = fs.readFileSync('./html/contact.html', 'utf8');
+
+  const pageTitle = 'Contact us';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+  res.send(topWithTitle + contact + bottom); // vienas html failass
 });
 
 //paleidzia serveri ir paraso kad viskas gerai
---
+++ 040/public/app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', () => {
+        button.innerText = 'Aciu jusu uzklausa gavome';
+        console.log('Button clicked')
+    })
+}
